3de987db00d64c9861b0c0fcf3da06f69e1c9ace
Hesdras Francisco <108990874+flleuma@users.noreply.github.com>
2023-04-14 09:25:29 -0300

    mapIF

--- pages/Home/js/mapFunction.js
+++ pages/Home/js/mapFunction.js
@@ -34,25 +34,25 @@ if (navigator.geolocation) {
         var fixedLocation = L.latLng(LOCAL_COMPANY.lat, LOCAL_COMPANY.long);
         var userLocation = L.latLng(latitude, longitude);
 
-        if (fixedLocation.distanceTo(userLocation) <= 1000) {
+       // if (fixedLocation.distanceTo(userLocation) <= 1000) {
             // O usuário está dentro do raio de 100 metros do ponto fixo
-            let userButton = document.getElementById("button_user");
-            userButton.disabled = false;
+            //let userButton = document.getElementById("button_user");
+           // userButton.disabled = false;
 
             
 
-        } else {
+        //} else {
             // O usuário está fora do raio de 3000 metros do ponto fixo
-            let userButton = document.getElementById("button_user");
-            userButton.style.backgroundColor = "#a9a9a9"
-            userButton.disabled = true;
-            userButton.addEventListener("click", () => {
-                var notyf = new Notyf();
-                notyf.error('Você está fora da área da empresa');
-            })
+            //let userButton = document.getElementById("button_user");
+            //userButton.style.backgroundColor = "#a9a9a9"
+            //userButton.disabled = true;
+           // userButton.addEventListener("click", () => {
+           //     var notyf = new Notyf();
+            //    notyf.error('Você está fora da área da empresa');
+            //})
 
 
-        }
+        //}
     });
 } else {
     // Caso o navegador não suporte a API Geolocation
